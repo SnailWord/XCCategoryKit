ba5fbca9e3fdbc20dd189f26eb9555a094b704a1
Yyy <yangyangy0423@126.com>
2020-03-05 17:15:54 +0800

    #Feature  XCView

---
+++ Example/XCView.h
@@ -0,0 +1,17 @@
+//
+//  XCView.h
+//  XCCategoryKit_Example
+//
+//  Created by James on 2020/3/5.
+//  Copyright © 2020 Yyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
