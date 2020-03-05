d040401f691119cbb60b5b7405be2f9340054ca1
Yyy <yangyangy0423@126.com>
2020-03-05 17:22:50 +0800

    #Feature  XCFlowView

---
+++ XCFlowView.h
@@ -0,0 +1,16 @@
+//
+//  XCFlowView.h
+//  Pods-XCCategoryKit_Example
+//
+//  Created by James on 2020/3/5.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCFlowView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XCFlowView.m
@@ -0,0 +1,20 @@
+//
+//  XCFlowView.m
+//  Pods-XCCategoryKit_Example
+//
+//  Created by James on 2020/3/5.
+//
+
+#import "XCFlowView.h"
+
+@implementation XCFlowView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
