350f089ce48f995840294baa606c08757495641a
Yyy <yangyangy0423@126.com>
2020-03-06 10:34:20 +0800

    #Feature  update

---
+++ XCCategoryKit/Classes/XCTools.h
@@ -0,0 +1,16 @@
+//
+//  XCTools.h
+//  Pods-XCCategoryKit_Example
+//
+//  Created by James on 2020/3/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCTools : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
