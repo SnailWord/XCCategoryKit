7e69cbb4b328f3de64c3b5d5cbb5e9de48e1c71a
Yyy <yangyangy0423@126.com>
2020-03-06 11:59:00 +0800

    #Feature  update

--- XCCategoryKit/Classes/mainView/XCCategoryTools.m
+++ XCCategoryKit/Classes/mainView/XCCategoryTools.m
@@ -1,17 +1,13 @@
 //
-//  XCTest.h
+//  XCCategoryTools.m
 //  XCCategoryKit_Example
 //
 //  Created by James on 2020/3/6.
 //  Copyright © 2020 Yyy. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "XCCategoryTools.h"
 
-NS_ASSUME_NONNULL_BEGIN
-
-@interface XCTest : NSObject
+@implementation XCCategoryTools
 
 @end
-
-NS_ASSUME_NONNULL_END
