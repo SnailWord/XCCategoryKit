943b84191ca341ebac7a4e55389f8f311732732b
Yyy <yangyangy0423@126.com>
2020-03-06 15:32:04 +0800

    #Feature  update

--- XCCategoryKit/Classes/mainView/XCYYY.h
+++ XCCategoryKit/Classes/mainView/XCYYY.h
@@ -1,13 +1,17 @@
 //
-//  XCOne.m
+//  XCYYY.h
 //  XCCategoryKit_Example
 //
 //  Created by James on 2020/3/6.
 //  Copyright © 2020 Yyy. All rights reserved.
 //
 
-#import "XCOne.h"
+#import <Foundation/Foundation.h>
 
-@implementation XCOne
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCYYY : NSObject
 
 @end
+
+NS_ASSUME_NONNULL_END
