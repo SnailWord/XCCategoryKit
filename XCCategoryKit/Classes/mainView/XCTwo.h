aaf431400927dec032f089a74a0d4e9debbcdca1
Yyy <yangyangy0423@126.com>
2020-03-06 12:58:33 +0800

    #Feature  update

---
+++ XCCategoryKit/Classes/mainView/XCTwo.h
@@ -0,0 +1,17 @@
+//
+//  XCTwo.h
+//  XCCategoryKit_Example
+//
+//  Created by James on 2020/3/6.
+//  Copyright © 2020 Yyy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCTwo : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XCCategoryKit/Classes/mainView/XCTwo.m
@@ -0,0 +1,13 @@
+//
+//  XCTwo.m
+//  XCCategoryKit_Example
+//
+//  Created by James on 2020/3/6.
+//  Copyright © 2020 Yyy. All rights reserved.
+//
+
+#import "XCTwo.h"
+
+@implementation XCTwo
+
+@end
